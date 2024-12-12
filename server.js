5b57ded3325ac7cbb1970ed1229b8c054ef9e6a9
rakeshcooper <rakeshmurali097@gmail.com>
2024-12-12 21:08:57 +0530

    updates

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const PORT = process.env.PORT || 3000
 const path = require('path')
 const fs = require('fs')
 const ejs = require('ejs')
+const bcrypt = require('bcrypt')
+const users = []
 let indexfile = fs.readFileSync(path.join('views','index.ejs'), 'utf-8');
 let loginfile = fs.readFileSync(path.join('views','login.ejs'), 'utf-8');
 let registerfile = fs.readFileSync(path.join('views','register.ejs'), 'utf-8');
- const headers = {
+const headers = {
                 "Access-Control-Allow-Origin": "*",
                 "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
                 "Access-Control-Max-Age": 2592000,
@@ -14,24 +16,67 @@ let registerfile = fs.readFileSync(path.join('views','register.ejs'), 'utf-8');
                 "Content-type": "text/html"
             };
 
-
-
 const server = http.createServer(async (req,res) => {
-    // res.writeHead(200,await headerData())
-    // res.end(ejs.render(templateString,{ name:'Coopers' }))
 
     try{
         if(req.url === '/login'){
             res.writeHead(200,headers)
-            res.end(ejs.render(loginfile,{ name:'Coopers' }))
+            res.end(ejs.render(loginfile))
+        } else if(req.url === '/login' && req.method === 'POST'){
+            res.writeHead(200,headers)
+            res.end(ejs.render(loginfile))
         } else if(req.url === '/register'){
             res.writeHead(200,headers)
-            res.end(ejs.render(registerfile,{ name:'Cooperss' }))
+            res.end(ejs.render(registerfile))
+        } else if(req.url === '/register' && req.method === 'POST'){
+            const body = await bodyPostdata(req,res)
+            const { name, email, password } = JSON.parse(body)
+            try {
+                // const hashedPassword = await bcrypt.hash(password,10)
+                users.push({
+                    id: Date.now().toString(),
+                    name: name,
+                    email: email,
+                    password: password
+                })
+                // res.redirect('/login')
+            } catch (error) {
+                console.log(error);
+                
+                // res.redirect('/register')
+            }
+            res.writeHead(200,headers)
+            res.end(ejs.render(users))
+            console.log(name);
+            
+                
         }
     } catch(err){
         console.log(err);
+         res.writeHead(200,headers)
+         res.end(JSON.stringify({message: `The error is ${err}`}))
     }
 })
+
+function bodyPostdata(req,res) {
+    try{
+        return new Promise((resolve, reject) => {
+            let body = ""
+            req.on("data",(chunk) => {
+                body += chunk.toString()
+            })
+
+            req.on("end", () => {
+                resolve(body)
+            })
+
+        })
+    }catch(err){
+        reject(err)
+        
+    }
+}
+
 
 server.listen(PORT,() => {
     console.log(`PORT is running ${PORT}`);
